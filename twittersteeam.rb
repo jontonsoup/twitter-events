8e86810d8fe43fa683aa0eae8bfc87cdb1ac4281
Jonathan Friedman <friedmanj98@gmail.com>
2012-04-17 15:51:54 -0500

    made output confirm to json (the end brackets need to be added though manually)

--- twittersteeam.rb
+++ twittersteeam.rb
@@ -1,10 +1,9 @@
 require 'rubygems'
-require 'active_support/all'
+require 'json'
+require 'twitter/json_stream'
 lib_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
 
-require 'twitter/json_stream'
-ticker = 0
 
 # EventMachine::run {
 #   stream = Twitter::JSONStream.connect(
@@ -14,34 +13,54 @@ ticker = 0
 #     #:content => 'track=ankees,yanks,nyy,bombers'
 #   )
 
+puts "{'tweets': ["
+
 EventMachine::run {
   stream = Twitter::JSONStream.connect(
     :path    => '/1/statuses/filter.json',
-    :auth    => 'LOGIN:PASSWORD',
+    :auth    => 'nutevents:northwestern',
     :method  => 'POST',
     :content => 'track=basketball,football,baseball,footy,soccer'
-  )
+    )
 
   stream.each_item do |item|
-    $stdout.print "#{item}\n"
+    $stdout.print "#{item},"
     $stdout.flush
-    ticker = ticker + 1
-    puts " \n" + ticker.to_s + "\n"
-  end
+
+  # This is for saving to the rails database
+  #   Tweet.create!({
+  #     :text => "",
+  #     :favorited => "",
+  #     :in_reply_to_user_id_str => "",
+  #     :geo => "",
+  #     :in_reply_to_screen_name => "",
+  #     :in_reply_to_status_id => "",
+  #     :in_reply_to_user_id => "",
+  #     :source => "",
+  #     :retweet_count => "",
+  #     :truncated => "",
+  #     :id_str => "",
+  #     :hashtags => "",
+  #     :retweeted => "",
+  #     :created_at => "",
+  #     :tweet_id => "",
+  #     :user_mentions => ""
+  #     })
+   end
 
   stream.on_error do |message|
-    $stdout.print "error: #{message}\n"
-    $stdout.flush
+    #$stdout.print "error: #{message}\n"
+    #$stdout.flush
   end
 
   stream.on_reconnect do |timeout, retries|
-    $stdout.print "reconnecting in: #{timeout} seconds\n"
-    $stdout.flush
+    #$stdout.print "reconnecting in: #{timeout} seconds\n"
+    #$stdout.flush
   end
 
   stream.on_max_reconnects do |timeout, retries|
-    $stdout.print "Failed after #{retries} failed reconnects\n"
-    $stdout.flush
+    #stdout.print "Failed after #{retries} failed reconnects\n"
+    #$stdout.flush
   end
 
   trap('TERM') {
@@ -49,4 +68,4 @@ EventMachine::run {
     EventMachine.stop if EventMachine.reactor_running?
   }
 }
-puts "The event loop has ended"
+#puts "The event loop has ended"
